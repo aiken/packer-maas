e2e0829c8f3717019943bcde6f42bd797291dae3
aiken <aiken.qi@gmail.com>
2024-04-29 08:53:17 +0800

    Update my-changes.sh

--- ubuntu/my-changes.sh
+++ ubuntu/my-changes.sh
@@ -1,2 +1,35 @@
 #`my-changes.sh` is a script you write which customizes the image from within the VM. For example, you can install packages using `apt-get`, call out to ansible, or whatever you want.
 # my-changes.sh 是你编写的一个脚本，用于在虚拟机内部定制镜像。例如，你可以使用 apt-get 安装软件包，调用 ansible，或者进行任何你想要的操作。这个脚本的作用是在镜像构建过程中执行特定的定制化任务，以确保最终生成的镜像符合你的需求。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
