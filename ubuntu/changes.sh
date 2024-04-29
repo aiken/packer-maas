aaae4445e78732f4bf481316eff0cd5a05d2beed
aiken <aiken.qi@gmail.com>
2024-04-29 08:53:04 +0800

    Update and rename changes.sh to my-changes.sh

--- ubuntu/changes.sh
+++
@@ -1,1 +0,0 @@
-#`my-changes.sh` is a script you write which customizes the image from within the VM. For example, you can install packages using `apt-get`, call out to ansible, or whatever you want.
